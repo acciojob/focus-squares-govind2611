08a4b1a48475f420240d577b008fd8d2e36f15e8
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-08-23 09:46:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+function changeOtherSquaresColor(square) {
+  var squares = document.getElementsByClassName('square');
+  for (var i = 0; i < squares.length; i++) {
+    if (squares[i] !== square) {
+      squares[i].classList.add('highlight');
+    }
+  }
+}
+
+function changeColorOnClick(square) {
+  var squares = document.getElementsByClassName('square');
+  if (square === squares[0] || square === squares[2]) {
+    for (var i = 0; i < squares.length; i++) {
+      if (squares[i] !== square) {
+        squares[i].classList.add('highlight');
+      }
+    }
+  }
+}
+
+function resetSquaresColor() {
+  var squares = document.getElementsByClassName('square');
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].classList.remove('highlight');
+  }
+}
